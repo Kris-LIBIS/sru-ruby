d6ee0d3844655b505870bb02bcec1edbff0eb165
Ed Summers <ehs@pobox.com>
2006-04-17 03:14:49 +0000

    some more docs

--- lib/sru/response.rb
+++ lib/sru/response.rb
@@ -14,18 +14,20 @@ module SRU
     end
 
     protected
-
-    # helper for doing xpath
+   
+    # get all nodes that match an xpath
     def xpath_all(path)
       return REXML::XPath.match(@doc, path, @@namespaces)
     end
 
+    # get the first node that matches an xpath
     def xpath_first(path)
       elements = xpath_all(path)
       return elements[0] if elements != nil
       return nil
     end
 
+    # get the text inside the first node that matches the xpath 
     def xpath(path)
       e = xpath_first(path)
       return e.text if e != nil
